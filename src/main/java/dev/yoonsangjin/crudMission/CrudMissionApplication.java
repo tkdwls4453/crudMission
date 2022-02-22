b76ec6fb121f875c95ef1f860f255bf9c86fe10f
Yoon321 <tkdwls734@gmail.com>
2022-02-22 16:37:47 +0900

    Initial commit

---
+++ src/main/java/dev/yoonsangjin/crudMission/CrudMissionApplication.java
@@ -0,0 +1,13 @@
+package dev.yoonsangjin.crudMission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yoonsangjin/crudMission/CrudMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yoonsangjin.crudMission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
